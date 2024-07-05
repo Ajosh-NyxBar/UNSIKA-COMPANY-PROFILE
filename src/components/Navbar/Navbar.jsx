44b4cf9df85c192b8d3fcc32960f3d1294c39cf7
MuhammadRifkyAkbar <2210631170088@student.unsika.ac.id>
2024-07-05 15:46:29 +0700

    second

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,18 @@
 import './navbar.css'
 import React from 'react';
-import logo from '../../assets/logo.png'
+import logo from '../../assets/log-unsika-removebg-preview.png'
 
 const Navbar = () => {
   return (
-    <nav>
-        <img src={logo} alt="logo" />
+    <nav className='container'>
+        <img src={logo} alt="logo" className='logo'/>
         <ul>
             <li>Home</li>
             <li>Program</li>
             <li>About</li>
             <li>Campus</li>
             <li>Testimonial</li>
-            <li>Contact</li>
+            <li><button className='btn'>Contact</button></li>
         </ul>
     </nav>
   );
