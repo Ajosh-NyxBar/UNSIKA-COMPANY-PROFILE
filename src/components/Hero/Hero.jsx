1e8b9d989d5728a8194f0f148929b9d3845230ba
MuhammadRifkyAkbar <2210631170088@student.unsika.ac.id>
2024-07-05 17:47:40 +0700

    add programs

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,21 @@
+import "./hero.css";
+import React from "react";
+import dark_arrow from "../../assets/dark-arrow.png";
+const Hero = () => {
+  return (
+    <div className="hero container">
+      <div className="hero-text">
+        <h1>We Ensure better education for a better world</h1>
+        <p>
+          We are a team of 10 students from different universities and we are
+          here to help you in your studies
+        </p>
+        <button className="btn">
+          Explore More <img src={dark_arrow} alt="" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
